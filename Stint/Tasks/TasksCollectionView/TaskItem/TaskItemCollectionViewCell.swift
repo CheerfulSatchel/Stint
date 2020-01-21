bbcc84110114be3f886c444ef4d7342b9f6ef50a
James Wu <therealjameswu@gmail.com>
2020-01-20 22:49:11 -0500

    Added checkbox-name container UIView.

--- Stint/Tasks/TasksCollectionView/TaskItem/TaskItemCollectionViewCell.swift
+++ Stint/Tasks/TasksCollectionView/TaskItem/TaskItemCollectionViewCell.swift
@@ -9,5 +9,37 @@
 import UIKit
 
 class TaskItemCollectionViewCell: UICollectionViewCell {
+  
+  var task: Task?
+  
+  var checkboxNameContainerView: UIView?
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+
+    contentView.backgroundColor = .purple
+    setupCheckboxNameContainer()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  private func setupCheckboxNameContainer() {
+    checkboxNameContainerView = UIView()
+    checkboxNameContainerView?.backgroundColor = .yellow
+    addSubview(checkboxNameContainerView!)
+    applyCheckboxNameContainerConstraints()
+
+  }
+  
+  private func applyCheckboxNameContainerConstraints() {
+    checkboxNameContainerView?.translatesAutoresizingMaskIntoConstraints = false
+    checkboxNameContainerView?.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
+    checkboxNameContainerView?.leftAnchor.constraint(equalTo: contentView.leftAnchor).isActive = true
+  checkboxNameContainerView?.rightAnchor.constraint(equalTo:contentView.rightAnchor).isActive = true
     
+    checkboxNameContainerView?.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 0.67).isActive = true
+  }
+  
 }
