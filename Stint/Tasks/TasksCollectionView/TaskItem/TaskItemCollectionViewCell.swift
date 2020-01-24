48a56d762e320b8e18515e04db83972aa9938bae
James Wu <therealjameswu@gmail.com>
2020-01-24 15:50:18 -0500

    Used NSLayoutConstraint and changed addTaskToView visibility to public.

--- Stint/Tasks/TasksCollectionView/TaskItem/TaskItemCollectionViewCell.swift
+++ Stint/Tasks/TasksCollectionView/TaskItem/TaskItemCollectionViewCell.swift
@@ -13,12 +13,14 @@ class TaskItemCollectionViewCell: UICollectionViewCell {
   var task: Task?
   
   var checkboxNameContainerView: UIView?
+  var timeUrgencyContainerView: UIView?
   
   override init(frame: CGRect) {
     super.init(frame: frame)
 
     contentView.backgroundColor = .purple
     setupCheckboxNameContainer()
+    setupTimeUrgencyContainer()
   }
   
   required init?(coder: NSCoder) {
@@ -27,15 +29,25 @@ class TaskItemCollectionViewCell: UICollectionViewCell {
   
   func addTaskToView() {
     let nameTextView: UITextView = {
-      let nameTextView = UITextView(frame:CGRect(x: 0.0, y: 0.0, width: 20, height: 20))
+      let nameTextView = UITextView()
       nameTextView.text = task?.name
-      nameTextView.font = UIFont(name: "HelveticaNeue", size: 10)
+      nameTextView.font = UIFont(name: "HelveticaNeue", size: 18)
       
       nameTextView.backgroundColor = .orange
       return nameTextView
     }()
-    checkboxNameContainerView?.addSubview(nameTextView)
     
+    nameTextView.translatesAutoresizingMaskIntoConstraints = false
+    checkboxNameContainerView?.addSubview(nameTextView)
+    nameTextView.widthAnchor.constraint(equalTo: (checkboxNameContainerView?.widthAnchor)!).isActive = true
+    nameTextView.heightAnchor.constraint(equalTo: (checkboxNameContainerView?.heightAnchor)!).isActive = true
+  }
+  
+  private func setupTimeUrgencyContainer() {
+    timeUrgencyContainerView = UIView()
+    timeUrgencyContainerView?.backgroundColor = .green
+    addSubview(timeUrgencyContainerView!)
+    applyTimeUrgencyContainerConstraints()
   }
   
   private func setupCheckboxNameContainer() {
@@ -45,14 +57,116 @@ class TaskItemCollectionViewCell: UICollectionViewCell {
     applyCheckboxNameContainerConstraints()
   }
   
+  private func applyTimeUrgencyContainerConstraints() {
+    timeUrgencyContainerView?.translatesAutoresizingMaskIntoConstraints = false
+    
+    let topConstraint = NSLayoutConstraint(
+      item: timeUrgencyContainerView!,
+      attribute: .top,
+      relatedBy: .equal,
+      toItem: checkboxNameContainerView!,
+      attribute: .bottom,
+      multiplier: 1.0,
+      constant: 0.0)
+    
+    let bottomConstraint = NSLayoutConstraint(
+      item: timeUrgencyContainerView!,
+      attribute: .bottom,
+      relatedBy: .equal,
+      toItem: contentView,
+      attribute: .bottom,
+      multiplier: 1.0,
+      constant: 0.0)
+    
+    let leftConstraint = NSLayoutConstraint(
+      item: timeUrgencyContainerView!,
+      attribute: .left,
+      relatedBy: .equal,
+      toItem: contentView,
+      attribute: .left,
+      multiplier: 1.0,
+      constant: 0.0)
+    
+    let rightConstraint = NSLayoutConstraint(
+      item: timeUrgencyContainerView!,
+      attribute: .right,
+      relatedBy: .equal,
+      toItem: contentView,
+      attribute: .right,
+      multiplier: 1.0,
+      constant: 0.0)
+    
+    let widthConstraint = NSLayoutConstraint(
+      item: timeUrgencyContainerView!,
+      attribute: .width,
+      relatedBy: .equal,
+      toItem: contentView,
+      attribute: .width,
+      multiplier: 1.0,
+      constant: 0.0)
+    
+    NSLayoutConstraint.activate([
+      topConstraint,
+      bottomConstraint,
+      leftConstraint,
+      rightConstraint,
+      widthConstraint,])
+  }
+  
   private func applyCheckboxNameContainerConstraints() {
     checkboxNameContainerView?.translatesAutoresizingMaskIntoConstraints = false
-    checkboxNameContainerView?.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
-    checkboxNameContainerView?.leftAnchor.constraint(equalTo: contentView.leftAnchor).isActive = true
-  checkboxNameContainerView?.rightAnchor.constraint(equalTo:contentView.rightAnchor).isActive = true
-    checkboxNameContainerView?.widthAnchor.constraint(equalTo: contentView.widthAnchor).isActive = true
-    checkboxNameContainerView?.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 0.85).isActive = true
 
+    let heightConstraint = NSLayoutConstraint(
+      item: checkboxNameContainerView!,
+      attribute: .height,
+      relatedBy: .equal,
+      toItem: contentView,
+      attribute: .height,
+      multiplier: 0.85,
+      constant: 0.0)
+
+    let widthConstraint = NSLayoutConstraint(
+      item: checkboxNameContainerView!,
+      attribute: .width,
+      relatedBy: .equal,
+      toItem: contentView,
+      attribute: .width,
+      multiplier: 1.0,
+      constant: 0.0)
+    
+    let topConstraint = NSLayoutConstraint(
+      item: checkboxNameContainerView!,
+      attribute: .top,
+      relatedBy: .equal,
+      toItem: contentView,
+      attribute: .top,
+      multiplier: 1.0,
+      constant: 0.0)
+  
+    let leftConstraint = NSLayoutConstraint(
+      item: checkboxNameContainerView!,
+      attribute: .left,
+      relatedBy: .equal,
+      toItem: contentView,
+      attribute: .left,
+      multiplier: 1.0,
+      constant: 0.0)
+    
+    let rightConstraint = NSLayoutConstraint(
+      item: checkboxNameContainerView!,
+      attribute: .right,
+      relatedBy: .equal,
+      toItem: contentView,
+      attribute: .right,
+      multiplier: 1.0,
+      constant: 0.0)
+    
+    NSLayoutConstraint.activate([
+      heightConstraint,
+      widthConstraint,
+      topConstraint,
+      leftConstraint,
+      rightConstraint,])
   }
   
 }
